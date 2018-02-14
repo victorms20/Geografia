e36d2c8a06f4a96cc1074f851b2b57b980499f35
Jordi Besalú Colomer <jordidaw1995@gmail.com>
2018-02-14 19:31:43 +0100

    Mapa test acabat.

--- Geografia/Scripts/js/javascript.js
+++ Geografia/Scripts/js/javascript.js
@@ -1,19 +1,20 @@
-function pintarMapa () {
+function pintarMapa() {
+
+
     for(var i = 1; i<42;i++){
         var IString = i.toString();
                   //  alert(document.getElementById("map").contentDocument);
         document.getElementById("map").contentDocument.getElementById("path"+IString).addEventListener('mouseover', function(e) {
             e.currentTarget.setAttribute('style', "fill: #ff7700");
+            document.getElementById("h3Comarca").innerHTML = "Comarca: " + e.currentTarget.dataset.comarca;
+            document.getElementById("h3Capital").innerHTML = "Capital: " + e.currentTarget.dataset.capital;
         });
                     
         document.getElementById("map").contentDocument.getElementById("path"+IString).addEventListener('mouseout', function(e) {
             e.currentTarget.setAttribute('style', "fill: #09e509; stroke-width: 1");
+            document.getElementById("h3Comarca").innerHTML = "Comarca: ";
+            document.getElementById("h3Capital").innerHTML = "Capital: ";
         });   
-                    
-        document.getElementById("map").contentDocument.getElementById("path"+IString).addEventListener('click',
-        function(e){
-        alert("Nom Comarca: " + e.currentTarget.dataset.comarca + " Capital: " + e.currentTarget.dataset.capital);          
-        });
         
     }
 }
