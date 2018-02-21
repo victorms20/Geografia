553248c9c097dd8b9413e40ef232831698151015
Jordi Besalú Colomer <jordidaw1995@gmail.com>
2018-02-21 19:37:03 +0100

    Solved Act 1 And Act2

--- Geografia/Scripts/js/Activitat2.js
+++ Geografia/Scripts/js/Activitat2.js
@@ -4,48 +4,9 @@ var contadorPositiu = 0;
 var contadorNegatiu = 0;
 var puntPerEncert = 2.4490243902439024390243902439024;
 var suma = 0;
+var pathComarcaCapital;
 
-function pintarMapa() {
-    ompleArray();
-    var spplitedPath = obtenirComarca();
-
-    //For per afegir event listener a totes les comarques del svg
-    for (var i = 1; i < 42; i++) {
-        var IString = i.toString();
-
-        //Afegim Mouseover
-        document.getElementById("map").contentDocument.getElementById("path" + IString).addEventListener('mouseover', function (e) {
-            e.currentTarget.setAttribute('style', "fill: #ff7700");
-        });
-
-        //Afegim MouseOut
-        document.getElementById("map").contentDocument.getElementById("path" + IString).addEventListener('mouseout', function (e) {
-            e.currentTarget.setAttribute('style', "fill: #086800; stroke-width: 1");
-        });
-
-        //Afegim click Event
-        document.getElementById("map").contentDocument.getElementById("path" + IString).addEventListener('click', function (e) {
-            var a = e.currentTarget.dataset.comarca;
-            //Cridem a la funcio per comprovar si el que hem clicat és correcte
-            comprovarComarca(a, spplitedPath);
-            //Pintem el marcador
-            document.getElementById("marcador_encerts").innerHTML = "<h4> " + contadorPositiu + "/" + (contadorNegatiu + contadorPositiu) + " </h4 > ";
-            suma = suma + puntPerEncert;
-            document.getElementById("barraProgress1").setAttribute('style', "width: " + suma + "%")
-            document.getElementById("progressBarra").innerHTML = suma.toFixed(0) + "%"
-            spplitedPath = obtenirComarca();
-
-        });
-    }
-}
-
-function comprovarComarca(a, path) {
-    var comarcaActual = document.getElementById("h3Comarca").textContent.substr(25);
-
-    //Eliminem els event listsners de l'element clonant-lo
-    var element = document.getElementById("map").contentDocument.getElementById(path);
-    var new_element = element.cloneNode(true);
-    element.parentNode.replaceChild(new_element, element);
+function comprovarComarca(arrayComarca) {
 
     //PINTA AMB COLOR VERD O VERMELL SEGONS SI ÉS CORRECTE O NO
     if (comarcaActual == a) {
@@ -67,17 +28,21 @@ function obtenirComarca() {
         //Obtenim una comarca i el seu ID (path)
         var splitted = arrayComarques[random].split(";");
 
-        document.getElementById("h3Comarca").innerHTML = "<h4>Clicka sobre la comarca: </h4>" + '<div class="div_blanc"><h4 id="h4style">' + splitted[1] + "</h4></div>";
+        //Pintem la comarca obtenida pel random al HTML perque l'usuari escrigui la capital
+        document.getElementById("h3Comarca").innerHTML = "<h4>Escriu la capital de la comarca: </h4>" + '<div class="div_blanc"><h4 id="h4style">' + splitted[1] + "</h4></div>";
+        document.getElementById("map").contentDocument.getElementById(splitted[0]).setAttribute('style', "fill: #ff7700");
         arrayComarques.splice(random, 1);
-        var pathSplited = splitted[0];
+        
+        var capital = splitted[2];
 
-        return pathSplited;
+        return splitted;
     }
+
     else {
         var notaFinal = (contadorPositiu * 10) / 41;
         notaFinal = notaFinal.toFixed(2);
-        $('#notaFinal').html("<b>Has encertat " + contadorPositiu + ' de 41, la teva nota és: <span class="badge badge-secondary" >' + notaFinal + '</span ></b>');
-        $('#modalFinal').modal('show');
+        //$('#notaFinal').html("<b>Has encertat " + contadorPositiu + ' de 41, la teva nota és: <span class="badge badge-secondary" >' + notaFinal + '</span ></b>');
+        //$('#modalFinal').modal('show');
         return;
     }
 }
@@ -85,10 +50,13 @@ function obtenirComarca() {
 function ompleArray() {
     for (var i = 1; i < 42; i++) {
         var IString = i.toString();
-        var com = document.getElementById("map").contentDocument.getElementById("path" + IString).dataset.comarca;
 
-        arrayComarques.push("path" + IString + ";" + com);
+        var comarca = document.getElementById("map").contentDocument.getElementById("path" + IString).dataset.comarca;
+        var capital = document.getElementById("map").contentDocument.getElementById("path" + IString).dataset.capital;
+
+        arrayComarques.push("path" + IString + ";" + comarca + ";" + capital);
 
     }
+    pathComarcaCapital = obtenirComarca();
 }
-window.onload = pintarMapa;
+window.onload = ompleArray;
--- Geografia/Scripts/js/javascript.js
+++ Geografia/Scripts/js/javascript.js
@@ -1,94 +1,24 @@
-//Array que conté totes les comarques i el seu "id de path"
-var arrayComarques = [];
-var contadorPositiu = 0;
-var contadorNegatiu = 0;
-var puntPerEncert = 2.4490243902439024390243902439024;
-var suma = 0;
-
 function pintarMapa() {
-    ompleArray();
-    var spplitedPath = obtenirComarca();
+    document.getElementById("h4Comarca").innerHTML = "<h4>Comarca: </h4>";
+    document.getElementById("h4Capital").innerHTML = "<h4>Capital: </h4>";
 
-    //For per afegir event listener a totes les comarques del svg
-    for (var i = 1; i < 42; i++) {
+
+    for (var i = 1; i < 41; i++) {
         var IString = i.toString();
-
-        //Afegim Mouseover
+        //  alert(document.getElementById("map").contentDocument);
         document.getElementById("map").contentDocument.getElementById("path" + IString).addEventListener('mouseover', function (e) {
             e.currentTarget.setAttribute('style', "fill: #ff7700");
+            document.getElementById("h4Comarca").innerHTML = "<h4>Comarca: </h4>" + '<div class="div_blanc_repas"><h4 id="h4style">' + e.currentTarget.dataset.comarca + "</h4></div>";
+            document.getElementById("h4Capital").innerHTML = "<h4>Capital: </h4>" + '<div class="div_blanc_repas"><h4 id="h4style">' + e.currentTarget.dataset.capital + "</h4></div>";
         });
 
-        //Afegim MouseOut
         document.getElementById("map").contentDocument.getElementById("path" + IString).addEventListener('mouseout', function (e) {
             e.currentTarget.setAttribute('style', "fill: #086800; stroke-width: 1");
+            document.getElementById("h4Comarca").innerHTML = "<h4>Comarca: </h4>" + '<div class="div_blanc_repas">' + "</div>";
+            document.getElementById("h4Capital").innerHTML = "<h4>Capital: </h4>" + '<div class="div_blanc_repas">' + "</div>";
         });
 
-        //Afegim click Event
-        document.getElementById("map").contentDocument.getElementById("path" + IString).addEventListener('click', function (e) {
-            var a = e.currentTarget.dataset.comarca;
-            //Cridem a la funcio per comprovar si el que hem clicat és correcte
-            comprovarComarca(a, spplitedPath);
-            //Pintem el marcador
-            document.getElementById("marcador_encerts").innerHTML = "<h4> " + contadorPositiu + "/" + (contadorNegatiu + contadorPositiu) + " </h4 > ";
-            suma = suma + puntPerEncert;
-            document.getElementById("barraProgress1").setAttribute('style', "width: " + suma + "%")
-            document.getElementById("progressBarra").innerHTML = suma.toFixed(0) + "%"
-            spplitedPath = obtenirComarca();
-
-        });
     }
 }
 
-function comprovarComarca(a, path) {
-    var comarcaActual = document.getElementById("h3Comarca").textContent.substr(25);
-
-    //Eliminem els event listsners de l'element clonant-lo
-    var element = document.getElementById("map").contentDocument.getElementById(path);
-    var new_element = element.cloneNode(true);
-    element.parentNode.replaceChild(new_element, element);
-
-    //PINTA AMB COLOR VERD O VERMELL SEGONS SI ÉS CORRECTE O NO
-    if (comarcaActual == a) {
-        document.getElementById("map").contentDocument.getElementById(path).setAttribute('style', "fill: #12E700");
-        contadorPositiu++;
-    }
-    else {
-        document.getElementById("map").contentDocument.getElementById(path).setAttribute('style', "fill: #E71000");
-        contadorNegatiu++;
-    }
-}
-
-function obtenirComarca() {
-    var llargadaArray = arrayComarques.length;
-    //Creem un random
-    if (llargadaArray > 0) {
-        var random = Math.floor(Math.random() * llargadaArray);
-
-        //Obtenim una comarca i el seu ID (path)
-        var splitted = arrayComarques[random].split(";");
-
-        document.getElementById("h3Comarca").innerHTML = "<h4>Clicka sobre la comarca: </h4>" + '<div class="div_blanc"><h4 id="h4style">' + splitted[1] + "</h4></div>";
-        arrayComarques.splice(random, 1);
-        var pathSplited = splitted[0];
-
-        return pathSplited;
-    }
-    else {
-        var notaFinal = (contadorPositiu * 10) / 41;
-        notaFinal = notaFinal.toFixed(2);
-        $('#notaFinal').html("<b>Has encertat " + contadorPositiu + ' de 41, la teva nota és: <span class="badge badge-secondary" >' + notaFinal + '</span ></b>');
-        $('#modalFinal').modal('show');
-        return;
-    }
-}
-
-function ompleArray() {
-    for (var i = 1; i < 42; i++) {
-        var IString = i.toString();
-        var com = document.getElementById("map").contentDocument.getElementById("path" + IString).dataset.comarca;
-
-        arrayComarques.push("path" + IString + ";" + com);
-
-    }
-}
 window.onload = pintarMapa;
